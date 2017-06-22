a6cfde6d978a8483b529805882da85c47cbd8308
unknown <Thousand@123899585@qq.com>
2017-06-22 23:38:21 +0800

    order list

--- app/scenes/Orders.js
+++ app/scenes/Orders.js
@@ -22,6 +22,11 @@ const { width: Width, height: Height } = Dimensions.get('window');
 class Orders extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            requestList: [],
+            inviteList: []
+        }
     }
 
     onChangeTab = (key) => {
@@ -32,19 +37,59 @@ class Orders extends React.Component {
         console.log('onTabClick key = ' + key);
     }
 
+    renderRequestRow = (rowData) => {
+        return (
+            <View>
+                <Text>
+                    申请列表
+                </Text>
+            </View>
+        )
+    }
+
+    renderInviteRow = (rowData) => {
+        return (
+            <View>
+                <Text>
+                    邀请列表
+                </Text>
+            </View>
+        )
+    }
+
+    renderSeparatorComponent() {
+        return (
+            <WhiteSpace style={{ backgroundColor: '#e9e9ef' }} />
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Tabs defaultActiveKey='1' onChange={this.onChangeTab} onTabClick={this.onTabClick}>
                     <TabPane tab='申请' key='1'>
-                        <Text>
-                            申请
-                        </Text>
+                        {
+                            this.state.requestList.length > 0 ?
+                                <FlatList
+                                    data={this.state.requestList}
+                                    renderItem={this.renderRequestRow.bind(this)}
+                                    ItemSeparatorComponent={this.renderSeparatorComponent.bind(this)}
+                                />
+                                :
+                                <Text style={{ alignSelf: 'center' }}>您的申请列表空空如也</Text>
+                        }
                     </TabPane>
                     <TabPane tab='邀约' key='2'>
-                        <Text>
-                            邀请
-                        </Text>
+                        {
+                            this.state.inviteList.length > 0 ?
+                                <FlatList
+                                    data={this.state.inviteList}
+                                    renderItem={this.renderInviteRow.bind(this)}
+                                    ItemSeparatorComponent={this.renderSeparatorComponent.bind(this)}
+                                />
+                                :
+                                <Text style={{ alignSelf: 'center' }}>您的邀约列表空空如也</Text>
+                        }
                     </TabPane>
                 </Tabs>
             </View>
